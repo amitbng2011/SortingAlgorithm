83e7d7d60c80878c3173afa81067097d8344cae2
Amit Singh <asingh844@apple.com>
2018-08-05 19:09:16 -0700

    Added bubble sort implementation

---
+++ src.main.java/src/main/java/sorting/bubblesort/BubbleSort.java
@@ -0,0 +1,30 @@
+package src.main.java.sorting.bubblesort;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] bubbleSortArr = {5,1,4,2,8};
+		sort(bubbleSortArr);
+		printArray(bubbleSortArr);
+	}
+
+	private static void  sort(int[] bubbleSortArr) {
+		for(int i = 0; i<bubbleSortArr.length-1; i++) {
+			boolean isSwapped=false;
+			for(int j=0; j<bubbleSortArr.length-i-1;j++) {
+				if(bubbleSortArr[j]>bubbleSortArr[j+1]) {
+					int temp = bubbleSortArr[j];
+					bubbleSortArr[j] = bubbleSortArr[j+1];
+					bubbleSortArr[j+1]=temp;
+					isSwapped=true;
+				}
+			}
+			if(!isSwapped) break;
+		}
+	}
+	
+	private static void printArray(int[] arr) {
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+}
